3f35abaf5cb154350facf7025e90ad6dcaa2e764
Rob Broadwell <hello@robbroadwell.com>
2018-04-27 12:53:50 -0600

    Refactor

--- inspire/ViewController.swift
+++ inspire/ViewController.swift
@@ -69,18 +69,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 75
     }
     
-    // MARK:- Networking / Data
+    // MARK:- Data
 
+    /// Obtains an Apache .log file and populates CoreData with sequence results.
+    /// CPU and memory intensive: should occur on a background thread.
     func fetchSequences(withContext context: NSManagedObjectContext) {
         
         // private managed object context
         let privateMOC = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
         privateMOC.parent = context
-
-        let entity = NSEntityDescription.entity(forEntityName: "Sequences", in: context)
         
         // remove anything residual from CoreData
-        purgeCoreData(context: privateMOC)
+        purgeCoreData(withContext: privateMOC)
         
         // tracks active sequences for each IP, example:
         // ["192.168.1.1": ["/example/"]
@@ -92,10 +92,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         do {
             
             // get the .log file from the server
-            let html = try String(contentsOf: url, encoding: .utf8)
+            let log = try String(contentsOf: url, encoding: .utf8)
             
             // split it up by new lines (\n)
-            let lines = html.lines
+            let lines = log.lines
             
             // iterate through the lines of the log file
             for (index, row) in lines.enumerated() {
@@ -110,11 +110,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 let split = row.split(separator: " ")
                 
                 // get the ip and page
-                // TODO: this works but could be better
                 let ip = String(split[0])
                 let path = String(split[6])
                 
-                // if this is the first sequence for an IP create a tracking array
+                // if first sequence for an IP create a tracking array
                 if sequences[ip] == nil {
                     sequences[ip] = [String]()
                 }
@@ -127,33 +126,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 
                 if sequence.count == 3 {
                     
-                    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Sequences")
-                    
                     // check if the sequence already exists
-                    let pathPredicate1 = NSPredicate(format: "path_1 = %@", sequence[0])
-                    let pathPredicate2 = NSPredicate(format: "path_2 = %@", sequence[1])
-                    let pathPredicate3 = NSPredicate(format: "path_3 = %@", sequence[2])
-                    let predicate = NSCompoundPredicate(type: NSCompoundPredicate.LogicalType.and, subpredicates: [pathPredicate1, pathPredicate2, pathPredicate3])
-                    
-                    request.predicate = predicate
-                    request.returnsObjectsAsFaults = false
-                    let result = try privateMOC.fetch(request)
-                    
-                    if result.count > 0 {
-                        // this sequence already exists -> increment the count
-                        for data in result as! [NSManagedObject] {
-                            data.setValue(data.value(forKey: "count") as! Int + 1, forKey: "count")
-                        }
+                    if let existingSequence = getSequence(sequence, withContext: privateMOC) {
                         
+                        // if it does update the count
+                        incrementSequence(existingSequence)
+
                     } else {
-                        // this sequence is new -> create a CoreData managed object
-                        let newSequence = NSManagedObject(entity: entity!, insertInto: privateMOC)
                         
-                        newSequence.setValue(sequence[0], forKey: "path_1")
-                        newSequence.setValue(sequence[1], forKey: "path_2")
-                        newSequence.setValue(sequence[2], forKey: "path_3")
-                        newSequence.setValue(1, forKey: "count")
-                        
+                        // if it doesn't create a new entity
+                        createSequence(sequence, withContext: privateMOC)
+
                     }
                     
                     // pop the first path from the sequence to continue tracking
@@ -169,10 +152,54 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    // MARK:- CoreData
+    /// Attempts to retrieve a single sequence managed object from CoreData.
+    func getSequence(_ sequence: [String], withContext context: NSManagedObjectContext) -> [NSManagedObject]? {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Sequences")
+        
+        let pathPredicate1 = NSPredicate(format: "path_1 = %@", sequence[0])
+        let pathPredicate2 = NSPredicate(format: "path_2 = %@", sequence[1])
+        let pathPredicate3 = NSPredicate(format: "path_3 = %@", sequence[2])
+        let predicate = NSCompoundPredicate(type: NSCompoundPredicate.LogicalType.and, subpredicates: [pathPredicate1, pathPredicate2, pathPredicate3])
+        
+        request.predicate = predicate
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            if let results = try context.fetch(request) as? [NSManagedObject] {
+                if results.count > 0 {
+                    return results
+                }
+            }
+            
+        } catch {
+            print("Something went wrong.")
+        }
+        
+        return nil
+    }
+    
+    /// Creates a new sequence in CoreData.
+    func createSequence(_ sequence: [String], withContext context: NSManagedObjectContext) {
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Sequences", in: context)
+        let newSequence = NSManagedObject(entity: entity!, insertInto: context)
+        
+        newSequence.setValue(sequence[0], forKey: "path_1")
+        newSequence.setValue(sequence[1], forKey: "path_2")
+        newSequence.setValue(sequence[2], forKey: "path_3")
+        newSequence.setValue(1, forKey: "count")
+    }
+    
+    /// Increments the count attribute on a sequence managed object.
+    func incrementSequence(_ sequence: [NSManagedObject]) {
+        for element in sequence {
+            element.setValue(element.value(forKey: "count") as! Int + 1, forKey: "count")
+        }
+    }
 
     /// Wipes all records from CoreData.
-    private func purgeCoreData(context: NSManagedObjectContext) {
+    private func purgeCoreData(withContext context: NSManagedObjectContext) {
         
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Sequences")
         request.returnsObjectsAsFaults = false
@@ -189,7 +216,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
 
-    /// Loads all sequences from CoreData for the TableView.
+    /// Loads all sequences from CoreData into the TableView.
+    /// Updates the UI: must occur on the main thread.
     private func loadSequences(withContext context: NSManagedObjectContext) {
 
         // core data fetch request
